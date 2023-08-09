f3bc71cf692f1688d4c1c2f23133623384cfc814
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-08-09 15:45:20 -0500

    Update consecionarias.py

--- pages/consecionarias.py
+++ pages/consecionarias.py
@@ -1,22 +1,17 @@
 import streamlit as st
-from streamlit_echarts import st_echarts
-from streamlit_echarts import st_pyecharts
-from streamlit_timeline import st_timeline
-import plotly.express as px
-import pandas as pd
-import numpy as np
-import plotly.figure_factory as ff
-import altair as alt
-import pydeck as pdk
-import matplotlib.pyplot as plt
-import datetime
-import base64
-from pyecharts import options as opts
-from pyecharts.charts import Graph
-
 
 st.set_page_config(layout="wide", page_title="Novus Legal ⚖️", page_icon="⚖️")
 
 st.title('Novus Legal ⚖️')
 
-st.header("Defensa Jurídica ante Consecionarios Viales 🚧")
+st.header("🛣 Defensa Jurídica ante Consecionarios Viales 🚧")
+
+st.markdown(
+  """
+  - 🔎_    Dimensiona cuantos riesgos implican las obras de conseciones viales en tu territorio
+  - 🛒_    Presupuesta tus potenciales pérdidas y activa plan jurídico de defensa
+  - 🧾_    Monitoriza en tiempo real el estado de tu defensa y la conquista de tus derechos
+  
+  EMPIEZA TU 🔎 DIAGNÓSTICO AHORA 🕰
+  """
+)
