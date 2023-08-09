3d85c1ea0d812d7f24b532fda08654d2b66ff26b
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-08-09 15:35:26 -0500

    Create consecionarias.py

---
+++ pages/consecionarias.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from streamlit_echarts import st_echarts
+from streamlit_echarts import st_pyecharts
+from streamlit_timeline import st_timeline
+import plotly.express as px
+import pandas as pd
+import numpy as np
+import plotly.figure_factory as ff
+import altair as alt
+import pydeck as pdk
+import matplotlib.pyplot as plt
+import datetime
+import base64
+from pyecharts import options as opts
+from pyecharts.charts import Graph
+
+
+st.set_page_config(layout="wide", page_title="Novus Legal ⚖️", page_icon="⚖️")
+
+st.title('Novus Legal ⚖️')
+
+st.header("Plan de Defensa Jurídica ante Consecionarios Viales 🚧")
