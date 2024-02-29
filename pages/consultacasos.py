64efa716dc1821d7af5cc92604a51647bbd6f5fd
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-02-29 17:23:05 +0100

    Update consultacasos.py

--- pages/consultacasos.py
+++ pages/consultacasos.py
@@ -1,1 +1,7 @@
 import streamlit as st
+st.set_page_config(layout="wide", page_title="Novus Legal ⚖️", page_icon="⚖️")
+
+st.title('Novus Legal ⚖️')
+
+st.header("Consulta el estado de tu proceso jurídico 🔎")
+
