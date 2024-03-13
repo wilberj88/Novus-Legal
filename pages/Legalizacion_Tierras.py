b9caae1477e89ecc17959f346e2a492c3deb7f42
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-03-13 16:16:12 +0100

    Update Legalizacion_Tierras.py

--- pages/Legalizacion_Tierras.py
+++ pages/Legalizacion_Tierras.py
@@ -1,8 +1,20 @@
 import streamlit as st
+import folium
+from streamlit_folium import st_folium
+
 st.set_page_config(layout="wide", page_title="Novus Legal ⚖️", page_icon="⚖️")
 
-st.title('Novus Legal ⚖️')
+st.title('Novus Legal ⚖️ Legalización de Tierras 🗺️')
 
-st.header("Consulta el estado de tu proceso jurídico 🔎")
+st.header("Experiente jurídico 🔎 #001-SAN-COL-2024")
+
 
 col1, col2 = st.columns(2)
+col1.write("Propietarios: Isbelia Hernández, Wilber Jiménez Viloria y Wilber Jiménez Hernández")
+col1.write("Área: 42 hectáreas")
+col1.write("Número de Matrícula Inmobiliario: _______")
+col1.write("Número Predial: ______")
+
+
+
+st.subheader ("Etapas de legalización")
