fc65b84378e6e8cafa211820dbf07d467948434d
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-03-13 16:03:51 +0100

    Create Legalizacion_Tierras.py

---
+++ pages/Legalizacion_Tierras.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.set_page_config(layout="wide", page_title="Novus Legal ⚖️", page_icon="⚖️")
+
+st.title('Novus Legal ⚖️')
+
+st.header("Consulta el estado de tu proceso jurídico 🔎")
+
+col1, col2 = st.columns(2)
